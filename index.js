067c180d44f45375723dae70d84c2828f79dcaad
Andres Sorto <andresismaelsortozuniga@gmail.com>
2025-01-10 17:57:56 -0600

    Environments agregado

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
+const express = require('express');
+require('dotenv').config();
 
-console.log('Hola mundo!!');
+// Crear el servidor de express
+const app = express();
+
+// Llamar varianble puerto en .env
+const port = process.env.PORT
+
+// Directorio publico
+app.use( express.static('public') );
+
+// Rutas
+// app.get('/', (req, res) => {
+
+//     res.json({
+//         "ok": true
+//     })
+// })
+
+// Escuchar las peticiones
+app.listen( port, () => {
+    console.log(`Servidor corriendo en puerto: ${port}`);
+} );
+
